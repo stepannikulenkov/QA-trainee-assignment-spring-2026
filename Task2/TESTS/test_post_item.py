4692cd46e7266fd1fd422968ca686344e3b669f3
Stepan Nikulenkov <stepannikulenkov@yandex.ru>
2026-04-05 12:47:24 +0300

    Task2

--- Task2/TESTS/test_post_item.py
+++ Task2/TESTS/test_post_item.py
@@ -1,4 +1,5 @@
 import requests
+import pytest
 
 def test_item_data(api_client, new_item): #проверка на совпадение данных с отправленными
     item_id = new_item[1]
@@ -15,19 +16,182 @@ def test_item_data(api_client, new_item): #проверка на совпаде�
     assert data["sellerId"] == payload["sellerID"]
     assert data["statistics"] == payload["statistics"]
 
-def test_item_in_statistics(api_client, new_item):
+def test_item_in_statistics(api_client, new_item): #объявление в массиве статистики
 
     sellerID = new_item[2]
-    item_id = (new_item[1])
+    item_id = new_item[1]
+    payload = new_item[0]
 
     response = api_client.get_statistics(sellerID)
     assert response.status_code == 200
 
     data = response.json()
 
-    print(response.json())
     assert any(x["id"] == (item_id) for x in data)
     assert any(x["sellerId"] == (sellerID) for x in data)
+    assert any(x["name"] == (payload["name"]) for x in data)
+    assert any(x["price"] == (payload["price"]) for x in data)
+    assert any(x["statistics"] == (payload["statistics"]) for x in data)
 
+
+@pytest.mark.parametrize(
+    "new_item",
+    [111111, 999999],
+    indirect=True 
+)
+def test__boarders(api_client, new_item):
+    payload, item_id, sellerID = new_item
+
+    response = api_client.get_item(item_id)
+    assert response.status_code == 200
+
+    data = response.json()[0]
+    assert data["sellerId"] == sellerID
+
+def test_two_items_same_seller(api_client):
+    sellerID = 185535
+
+    payload = {
+        "sellerID": sellerID,
+        "name": "wqdf",
+        "price": 121,
+        "statistics": {
+            "likes": 1542,
+            "viewCount": 8765,
+            "contacts": 231
+        }
+    }
+
+    response1 = api_client.create_item(payload)
+    assert response1.status_code == 200
+    item_id_1 = response1.json()["status"].split()[-1]
+
+    response2 = api_client.create_item(payload)
+    assert response2.status_code == 200
+    item_id_2 = response2.json()["status"].split()[-1]
+
+    response = api_client.get_statistics(sellerID)
+    assert response.status_code == 200
+
+    data = response.json()
+
+    ids = [x["id"] for x in data]
+
+    assert item_id_1 in ids
+    assert item_id_2 in ids
+
+@pytest.mark.parametrize("name", [None, ""])
+def test_without_name(api_client, name):
     
+    payload = {
+        "sellerID": 185537,
+        "name": name,
+        "price": 121,
+        "statistics": {
+            "likes": 1542,
+            "viewCount": 8765,
+            "contacts": 231
+        }
+    }
+    response = api_client.create_item(payload)
+    assert response.status_code == 400
+    print(response.json())
+
+@pytest.mark.parametrize("sellerID", [None, "abc"])
+def test_without_sellerID(api_client, sellerID):
     
+    payload = {
+        "sellerID": sellerID,
+        "name": "iphone",
+        "price": 121,
+        "statistics": {
+            "likes": 1542,
+            "viewCount": 8765,
+            "contacts": 231
+        }
+    }
+    response = api_client.create_item(payload)
+    assert response.status_code == 400
+    print(response.json())
+
+@pytest.mark.parametrize("price", [None, -12345, "abc"])
+def test_without_sellerID(api_client, price):
+    
+    payload = {
+        "sellerID": 185537,
+        "name": "iphone",
+        "price": price,
+        "statistics": {
+            "likes": 1542,
+            "viewCount": 8765,
+            "contacts": 231
+        }
+    }
+    response = api_client.create_item(payload)
+    assert response.status_code == 400
+    print(response.json())
+
+def test_without_statistics(api_client):
+
+    payload = {
+        "sellerID": 185537,
+        "name": "iphone",
+        "price": 1000,
+        "statistics": None
+    }
+     
+    response = api_client.create_item(payload)
+    assert response.status_code == 400 
+
+def test_post_without_body(api_client):
+
+    payload = {}
+
+    response = api_client.create_item(payload)
+    assert response.status_code == 400
+
+def test_statistics_likes(api_client):
+    
+    payload = {
+        "sellerID": 185537,
+        "name": "iphone",
+        "price": 1000,
+        "statistics": {
+            "likes": 0,
+            "viewCount": 8765,
+            "contacts": 231
+        }
+    }
+
+    response = api_client.create_item(payload)
+    assert response.status_code == 200 
+
+def test_statistics_likes_negativ(api_client):
+    
+    payload = {
+        "sellerID": 185537,
+        "name": "iphone",
+        "price": 1000,
+        "statistics": {
+            "likes": -1,
+            "viewCount": 8765,
+            "contacts": 231
+        }
+    }
+
+    response = api_client.create_item(payload)
+    assert response.status_code == 400 
+    
+def test_check_sellerID(api_client, new_item):
+
+    payload, item_id, sellerID  = new_item
+    response = api_client.get_item(item_id)
+    data = response.json()[0]
+
+    get_keys = set(data.keys())
+    post_keys = set(payload.keys())
+
+    print("keys only from GET:", get_keys - post_keys)
+    print("keys only from POST:", post_keys - get_keys)
+
+    assert data["sellerID"] == sellerID
--- Task2/conftest.py
+++ Task2/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 from utils.api_client import ApiClient
 
 
@@ -8,25 +9,24 @@ def api_client():
 
 
 @pytest.fixture
-def new_item(api_client):
+def new_item(api_client, request):
+    sellerID = getattr(request, "param", 145535)
+
     payload = {
-  "sellerID": 145535 ,
-  "name": "wqdf",
-  "price": -121,
-  "statistics": {
-    "likes": 1542,
-    "viewCount": 8765,
-    "contacts": 231
-  }
-}
+        "sellerID": sellerID,
+        "name": "wqdf",
+        "price": 121,
+        "statistics": {
+            "likes": 1542,
+            "viewCount": 8765,
+            "contacts": 231
+        }
+    }
 
     response = api_client.create_item(payload)
-
-    assert response.status_code == 200, f"Ошибка создания: {response.text}"
+    assert response.status_code == 200
 
     status_str = response.json()["status"]
     item_id = status_str.split()[-1]
-
-    sellerID = payload["sellerID"]
 
     return [payload, item_id, sellerID]
